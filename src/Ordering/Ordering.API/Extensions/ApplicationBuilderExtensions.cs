470de7fdda8cf99039345fddeb615071235a822f
GokhanErtgn <gokhanertogan@gmail.com>
2020-12-27 22:44:37 +0300

    rabbitmq consume and listener pipeline added

---
+++ src/Ordering/Ordering.API/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Ordering.API.RabbitMQ;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static EventBusRabbitMQConsumer Listener { get; set; }
+        public static IApplicationBuilder UseRabbitListener(this IApplicationBuilder app)
+        {
+            Listener = app.ApplicationServices.GetService<EventBusRabbitMQConsumer>();
+            var life = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+
+            life.ApplicationStarted.Register(OnStarter);
+            life.ApplicationStopping.Register(OnStopping);
+            return app;
+        }
+
+        private static void OnStarter()
+        {
+            Listener.Consume();
+        }
+
+        private static void OnStopping()
+        {
+            Listener.Disconnect();
+        }
+    }
+}
--- src/Ordering/Ordering.API/Startup.cs
+++ src/Ordering/Ordering.API/Startup.cs
@@ -1,25 +1,23 @@
 using AutoMapper;
+using EventBusRabbitMQ;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Ordering.API.Extensions;
+using Ordering.API.RabbitMQ;
 using Ordering.Application.Handlers;
 using Ordering.Core.Repositories;
 using Ordering.Core.Repositories.Base;
 using Ordering.Infrastructure.Data;
 using Ordering.Infrastructure.Repositories;
 using Ordering.Infrastructure.Repositories.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using RabbitMQ.Client;
 using System.Reflection;
-using System.Threading.Tasks;
 
 namespace Ordering.API
 {
@@ -51,8 +49,26 @@ namespace Ordering.API
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Order API", Version = "v1" });
             });
 
+            services.AddSingleton<IRabbitMQConnnection>(sp =>
+            {
+                var factory = new ConnectionFactory()
+                {
+                    HostName=Configuration["EventBus:HostName"]
+                };
 
+                if (!string.IsNullOrEmpty(Configuration["EventBus:UserName"]))
+                {
+                    factory.UserName = Configuration["EventBus:UserName"];
+                }
 
+                if (!string.IsNullOrEmpty(Configuration["EventBus:Password"]))
+                {
+                    factory.Password = Configuration["EventBus:Password"];
+                }
+                return new RabbitMQConnection(factory);
+            });
+
+            services.AddSingleton<EventBusRabbitMQConsumer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,6 +87,8 @@ namespace Ordering.API
             {
                 endpoints.MapControllers();
             });
+
+            app.UseRabbitListener();
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
